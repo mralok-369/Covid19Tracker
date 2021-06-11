f98a34f82899e62d6948cb19ea9ffd11bcfa4720
Alok Kumar <mralok369@gmail.com>
2021-06-11 14:36:17 +0530

    Netflix App by React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+import Card from './Card';
+import Sdata from './Sdata';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Props in react Project Netflix App
+ReactDOM.render(
+    <>
+        <Header />
+
+        {Sdata.map((val) => {
+            return (
+                <Card
+                    imgsrc={val.imgsrc}
+                    title={val.title}
+                    sname={val.sname}
+                    link={val.link}
+                />
+            );
+        })}
+
+    </>,
+    document.getElementById('root')
+);
+
+// Fat arrow function
+// const myname = (a, b) => a + b;
+
+// simple function for returning value of sdata
+// function ncard(val) {
+//     console.log(val);
+//     return (
+//         <Card
+//             imgsrc={val.imgsrc}
+//             title={val.title}
+//             sname={val.sname}
+//             link={val.link}
+//         />
+//     );
+// }
