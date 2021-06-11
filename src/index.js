dd939915f3a5b351ad2850eeae12e0a7c67e5ece
Alok Kumar <mralok369@gmail.com>
2021-06-11 14:42:02 +0530

    covid 19 tracker

--- src/index.js
+++ src/index.js
@@ -1,42 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
-import Card from './Card';
-import Sdata from './Sdata';
-import './index.css';
+import Covid from './components/Covid';
 
-// Props in react Project Netflix App
 ReactDOM.render(
     <>
-        <Header />
-
-        {Sdata.map((val) => {
-            return (
-                <Card
-                    imgsrc={val.imgsrc}
-                    title={val.title}
-                    sname={val.sname}
-                    link={val.link}
-                />
-            );
-        })}
-
+      <Covid />
     </>,
     document.getElementById('root')
 );
-
-// Fat arrow function
-// const myname = (a, b) => a + b;
-
-// simple function for returning value of sdata
-// function ncard(val) {
-//     console.log(val);
-//     return (
-//         <Card
-//             imgsrc={val.imgsrc}
-//             title={val.title}
-//             sname={val.sname}
-//             link={val.link}
-//         />
-//     );
-// }
